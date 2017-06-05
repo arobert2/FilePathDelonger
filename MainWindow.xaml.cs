b21c5835e9f058c06ef43db9c983c22e94010b42
Allen.Roberts <allen.roberts1985@gmail.com>
2017-06-04 21:53:05 -0400

    Moved away from static methods for managing files, started moving scan and copy functions off of the UI thread.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,11 +21,14 @@ namespace FilePathDelonger
     /// </summary>
     public partial class MainWindow : Window
     {
+        PathTools PathTools = new PathTools();   //PathTools
+
         public MainWindow()
         {
             InitializeComponent();
         }
-
+#region WPF Events
+        // FolderScan gets focus
         private void FolderScan_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox s = (TextBox)sender;
@@ -33,7 +36,7 @@ namespace FilePathDelonger
             s.Foreground = Brushes.Black;
             s.FontStyle = FontStyles.Normal;
         }
-
+        // FolderScan loses focus
         private void FolderScan_LostFocus(object sender, RoutedEventArgs e)
         {
             if (FolderScan.Text == "")
@@ -43,7 +46,7 @@ namespace FilePathDelonger
                 FolderScan.FontStyle = FontStyles.Italic;
             }
         }
-
+        // Output loses focus
         private void Output_LostFocus(object sender, RoutedEventArgs e)
         {
             if (FolderScan.Text == "")
@@ -53,7 +56,7 @@ namespace FilePathDelonger
                 Output.FontStyle = FontStyles.Italic;
             }
         }
-
+        // Browse button is clicked for FolderScan field.
         private void BrowseScan_Click(object sender, RoutedEventArgs e)
         {
             using (System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog())
@@ -63,7 +66,7 @@ namespace FilePathDelonger
                 FolderScan.Text = fbd.SelectedPath;
             }
         }
-
+        // Browse button is clicked for trhe Output field.
         private void BrowseOutput_Click(object sender, RoutedEventArgs e)
         {
             using (System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog())
@@ -73,24 +76,31 @@ namespace FilePathDelonger
                 Output.Text = fbd.SelectedPath;
             }
         }
-
+        // Quit button is clicked.
         private void bttnQuit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
-
+        // Fix button is clicked.
         private void FixButton_Click(object sender, RoutedEventArgs e)
         {
             if (Directory.Exists(FolderScan.Text))
             {
                 if (Directory.Exists(Output.Text))
                 {
+                    FileTree tree = new FileTree();  //FileTree
+                    Task.Run(() => { tree = PathTools.BuildTree(FolderScan.Text); });   //Build the tree
+                    Task.Run(() => { tree = PathTools.MarkCuts(tree, Output.Text, ""); });  //Mark cuts
+                    Task.Run(() => { PathTools.MoveFiles(tree, Output.Text); });    //move files
+                    
+
+                    /*
                     //Build the file tree
                     FileTree ft = PathTools.BuildTree(FolderScan.Text);
                     //Check for length and mark as needed
                     ft = PathTools.MarkCuts(ft, Output.Text, "");
                     //Move files that are marked.
-                    PathTools.MoveFiles(ft);
+                    PathTools.MoveFiles(ft);*/
                 }
                 else
                     MessageBox.Show("Path " + Output.Text + " does not exist.", "Folder not found!", MessageBoxButton.OK, MessageBoxImage.Error);
@@ -99,12 +109,15 @@ namespace FilePathDelonger
                 MessageBox.Show("Path " + FolderScan.Text + " does not exist.", "Folder not found!", MessageBoxButton.OK, MessageBoxImage.Error);
                 
         }
-
+        // About button is clicked.
         private void bttnAbout_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Author: Allen Roberts\n\n" +
-                "This program may cause data loss. Use at your own risk.\n\n" +
-                "This program is designed to move files and folders that exceed the Windows Explorer character limit to a specified location.");
+                "WARNING! This program moves files from one path to another, it does not copy, It is possible for you" +
+                "to misplace files when this is run on a path. If you cannot find a file check the log files in the destination folder to see where it was moved.\n\n" +
+                "This program is designed to move files and folders that exceed the Windows Explorer character limit to a specified location." +
+                "Paths that exceed this limit became very hard to back up and restore, and bypassing the path limit can cause OS instability.");
         }
     }
+    #endregion
 }
--- PathTools.cs
+++ PathTools.cs
@@ -9,136 +9,15 @@ using System.Threading;
 
 namespace FilePathDelonger
 {
-    public static class PathTools
+    public static class ExtensionMethods
     { 
-        /// <summary>
-        /// Build a FileTree from the selected location.
-        /// </summary>
-        /// <param name="path">Path to start with</param>
-        /// <returns>FileTree</returns>
-        public static FileTree BuildTree(string path)
-        {
-            FileTree DirectoryTree = new FileTree();                    //Create a tree to hold data
-            string[] dir;                                               //Subfolder paths
-
-            //Ignore access denied folders.
-            try
-            {
-                dir = Directory.GetDirectories(path);                   //Get child folders
-                DirectoryTree.Files.AddRange(Directory.GetFiles(path)); //Get child files             
-            }
-            catch(UnauthorizedAccessException uae)
-            {
-                return null;                                            //If not possible return null.
-            }
-            DirectoryTree.Path = path;                                  //Set Path in FileTree object
-            DirectoryTree.Directory = Path.GetFileName(path);           //Set directory name.
-
-            //Scan child directories
-            foreach (string d in dir)
-            {
-                FileTree subtree = BuildTree(d);                        //Create a new FileTree from the sub directory information.
-                if(subtree != null)                                     //Ensure that it was able to.
-                    DirectoryTree.Directories.Add(subtree);             //Add subtree to the main FileTree Directories property.
-            }
-
-            return DirectoryTree;                                       //Return new FileTree
-        }
-        /// <summary>
-        /// Get the file name from a string[] list of file paths.
-        /// </summary>
-        /// <param name="files">Files to chop</param>
-        /// <returns>filename</returns>
-        private static string[] ChopPath(string[] files)
-        {
-            List<string> chops = new List<string>();
-            foreach (string s in files)
-                chops.Add(Path.GetFileName(s));
-            return chops.ToArray();
-        }
-        /// <summary>
-        /// Sets AtLimit bool in FileTree object
-        /// </summary>
-        /// <param name="ft">FileTree</param>
-        /// <param name="path">Path to check</param>
-        public static FileTree MarkCuts(FileTree ft, string pathto, string lastcut)
-        {
-            int charlimit = 240 - pathto.Length;                    //maximum path size.
-            string lc = lastcut;                                    //Last cut for determining where to cut nust (path length - lastcutlength - destinationlength = maximum path size)
-            FileTree newTree = new FileTree()                       //Create a new tree that is identical to the previous except no child directores.
-            {
-                Path = ft.Path,
-                Directory = ft.Directory,
-                Files = ft.Files
-            };   
-            //If path is greater than charlimit
-            if(ft.Path.Length - lc.Length > charlimit)
-            {
-                newTree.AtLimit = true;                             //Mark the tree as a cut.
-                lc = ft.Path;                                       //update last cut with this path
-            }
-            //Check sub directories for cut mark.
-            foreach(FileTree f in ft.Directories)
-                newTree.Directories.Add(MarkCuts(f, pathto, lc));   //Run method against subtrees, add result to newTree sub directory (FileTree.Directories property)            
-            return newTree;                                         //return updated tree.
-        }
-        /// <summary>
-        /// Move files and folders that exceed limit
-        /// </summary>
-        /// <param name="ft">Root FileTree</param>
-        /// <param name="path">Path to move files to.</param>
-        public static void MoveFiles(FileTree ft, string path = @"C:\")
-        {          
-            foreach (FileTree f in ft.Directories)      //Go to end of branch before continuing.
-                MoveFiles(f, path);                     //restart loop
-
-            foreach (string f in ft.Files)              //look through files
-                if (f.Length - path.Length >= 240)      //Check to see if their path exceeds the limit
-                    Move(f, path);                      //Move file
-
-            if (ft.AtLimit)                             //If marked for cut
-                Move(ft.Path, path);                    //Move folder               
-        }
-        /// <summary>
-        /// Log information to a location
-        /// </summary>
-        /// <param name="path">Location of log file</param>
-        /// <param name="text">text to log</param>
-        private static void LogMove(string path, string text)
-        {
-            if (!File.Exists(path))     //If the log file doesn't exist
-                File.Create(path);      //Create a new log file
-
-            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(path, true))  //create a new StreamWriter
-            {
-                string write = "[".AppendTimeStamp() + "] " + text;                     //Add time stamp to log entry.
-                sw.WriteLine(write);                                                    //Write log
-                sw.Close();                                                             //Close file
-            }               
-        }
-        /// <summary>
-        /// Move a file or folder from one location to another without worrying about overwrite.
-        /// </summary>
-        /// <param name="inputpath">Object you wish to move</param>
-        /// <param name="outputpath">Where you want to move it.</param>
-        public static void Move(string inputpath, string outputpath)
-        {
-            string inp = inputpath.EnforceNoMatch(outputpath);                                          //adds a timestamp if the file or folder already exists at the desired move path.
-            string cpobj = Path.GetFileName(inp);                                                       //FileSystem object name without path.
-            string movefrom = Path.GetDirectoryName(inputpath);                                         //Path where FileSystem object comes from
-
-            Directory.Move(inputpath, outputpath.CapPath() + cpobj);                                    //Move the file/folder to the specified location
-            LogMove(outputpath.CapPath() + "FolderLog.txt", cpobj + " was moved from " + movefrom);     //Log the move 
-        }
-
-#region Extension Methods
         /// <summary>
         /// Adds a timestamp to a string if that FileSystem object already exists at the selected path.
         /// </summary>
         /// <param name="path">Path to object</param>
         /// <param name="outpath">Where you want to make sure it doesn't exist</param>
         /// <returns>string modified to not match</returns>
-        private static string EnforceNoMatch(this string path, string outpath)
+        public static string EnforceNoMatch(this string path, string outpath)
         {
             string checkfsobjname = Path.GetFileName(path);                             //get File System object name
             string checkfsobjnamenoext = "";                                            //System object name without extension
@@ -167,7 +46,7 @@ namespace FilePathDelonger
         /// </summary>
         /// <param name="st">String to change</param>
         /// <returns>Modified string</returns>
-        private static string AppendTimeStamp(this string st)
+        public static string AppendTimeStamp(this string st)
         {
             return st + DateTime.Now.ToString("yyyyMMddHHmmssfff");     //Add a timestamp to the folder
         }
@@ -176,13 +55,41 @@ namespace FilePathDelonger
         /// </summary>
         /// <param name="st">String to change</param>
         /// <returns>Modified string</returns>
-        private static string CapPath(this string st)
+        public static string CapPath(this string st)
         {
             if (st[st.Length - 1] == System.IO.Path.DirectorySeparatorChar) //If the last character is a \
                 return st;                                                  //return the original string
             return st + System.IO.Path.DirectorySeparatorChar;              //return a string with the \ added to the end.
         }
-#endregion
+        /// <summary>
+        /// Get the file name from a string[] list of file paths.
+        /// </summary>
+        /// <param name="files">Files to chop</param>
+        /// <returns>filename</returns>
+        public static string[] ChopPath(this string[] files)
+        {
+            List<string> chops = new List<string>();
+            foreach (string s in files)
+                chops.Add(Path.GetFileName(s));
+            return chops.ToArray();
+        }
+        /// <summary>
+        /// Log information to a location
+        /// </summary>
+        /// <param name="path">Location of log file</param>
+        /// <param name="text">text to log</param>
+        public static void LogMove(this string path, string text)
+        {
+            if (!File.Exists(path))     //If the log file doesn't exist
+                File.Create(path);      //Create a new log file
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(path, true))  //create a new StreamWriter
+            {
+                string write = "[".AppendTimeStamp() + "] " + text;                     //Add time stamp to log entry.
+                sw.WriteLine(write);                                                    //Write log
+                sw.Close();                                                             //Close file
+            }
+        }
     }
 
     public class FileTree
